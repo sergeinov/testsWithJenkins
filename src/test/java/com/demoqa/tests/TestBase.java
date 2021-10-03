96b5b7356d4acaabc53471fa6d75b063be834d52
tester <lookbugs@mail.ru>
2021-10-03 12:35:58 +0300

    added Attachments

--- src/test/java/com/demoqa/tests/TestBase.java
+++ src/test/java/com/demoqa/tests/TestBase.java
@@ -2,7 +2,9 @@ package com.demoqa.tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import com.demoqa.helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
@@ -22,6 +24,15 @@ public class TestBase {
         Configuration.browserCapabilities = capabilities;
         // url of selenoid
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
+    }
+
+    // add attachments into Allure report
+    @AfterEach
+    public void tearDown(){
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
 
 }
